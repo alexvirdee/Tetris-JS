1e1ce351b3e8ff331f2cf2e48837b37375f15e8c
Alexinder Virdee <alex.virdee08@gmail.com>
2017-05-07 12:56:24 -0400

    style and finished tetris game

--- tetris.js
+++ tetris.js
@@ -4,12 +4,23 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 
+function arenaSweep() {
+	let rowCount = 1;
+	outer: for (let y = arena.length - 1; y > 0; --y) {
+		for (let x = 0; x < arena[y].length; ++x) {
+			if (arena[y][x] === 0) {
+				continue outer;
+			}
+		}
 
-const matrix = [
-	[0, 0, 0],
-	[1, 1, 1],
-	[0, 1, 0],
-];
+		const row = arena.splice(y, 1)[0].fill(0)
+		arena.unshift(row);
+		++y;
+
+		player.score += rowCount * 10;
+		rowCount *= 2;
+	}
+}
 
 
 function collide(arena, player) {
@@ -36,7 +47,65 @@ function createMatrix(w, h) {
 	return matrix;
 }
 
+function createPiece(type) {
 
+	if (type === 'T') {
+		return [
+				[0, 0, 0],
+				[1, 1, 1],
+				[0, 1, 0],
+			];
+	} else if (type === 'O') {
+		return [
+
+				[2, 2],
+				[2, 2],
+
+		];
+	} else if (type === 'L') {
+		return [
+
+				[0, 3, 0],
+				[0, 3, 0],
+				[0, 3, 3],
+
+		];
+	} else if (type === 'J') {
+		return [
+
+				[0, 4, 0],
+				[0, 4, 0],
+				[4, 4, 0],
+
+		];
+	} else if (type === 'I') {
+		return [
+
+				[0, 5, 0, 0],
+				[0, 5, 0, 0],
+				[0, 5, 0, 0],
+				[0, 5, 0, 0],
+
+		];
+	} else if (type === 'S') {
+		return [
+
+				[0, 6, 6],
+				[6, 6, 0],
+				[0, 0, 0],
+
+		];
+	} else if (type === 'Z') {
+		return [
+
+				[7, 7, 0],
+				[0, 7, 7],
+				[0, 0, 0],
+
+		];
+	}
+
+}
 
 
 
@@ -54,7 +123,7 @@ function drawMatrix(matrix, offset) {
 	matrix.forEach((row, y) => {
 		row.forEach((value, x) => {
 			if (value !== 0) {
-				context.fillStyle = 'Red';
+				context.fillStyle = colors[value];
 				context.fillRect(x + offset.x, 
 								 y + offset.y, 
 								 1, 1);
@@ -81,7 +150,9 @@ function playerDrop() {
 		if (collide(arena, player)) {
 			player.pos.y--;
 			merge(arena, player);
-			player.pos.y = 0;
+			playerReset();
+			arenaSweep();
+			updateScore();
 		}
 		dropCounter = 0;
 }
@@ -91,6 +162,40 @@ function playerMove(dir) {
 	player.pos.x += dir;
 	if (collide(arena, player)) {
 		player.pos.x -= dir;
+	}
+}
+
+function playerReset() {
+
+	const pieces = 'ILJOTSZ';
+	player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+	player.pos.y = 0;
+	player.pos.x = (arena[0].length / 2 | 0) -
+					(player.matrix[0].length / 2 | 0);
+
+	if (collide(arena, player)) {
+		arena.forEach(row => row.fill(0));
+		player.score = 0;
+		updateScore();
+
+	}
+
+}
+
+
+
+function playerRotate(dir) {
+	const pos = player.pos.x
+	let offset = 1;
+	rotate(player.matrix, dir);
+	while (collide(arena, player)) {
+		player.pos.x += offset;
+		offset = -(offset + (offset > 0 ? 1 : -1));
+		if (offset > player.matrx[0].length) {
+			rotate(player.matrix, -dir);
+			player.pos.x = pos;
+			return;
+		}
 	}
 }
 
@@ -133,12 +238,28 @@ function update(time = 0) {
 	requestAnimationFrame(update);
 }
 
+function updateScore() {
+	document.getElementById('score').innerText = player.score;
+}
+
+const colors = [
+		null,
+		'#FF0D72',
+		'#0DC2FF',
+		'#0DFF72',
+		'#F538FF',
+		'#FF8E0D',
+		'#FFE138',
+		'#3877FF',
+	];
+
 const arena = createMatrix(12, 20);
 
 
 const player = {
-	pos: {x: 5, y: 5},
-	matrix: matrix,
+	pos: {x: 0, y: 0},
+	matrix: null,
+	score: 0, 
 }
 
 document.addEventListener('keydown', event => {
@@ -148,12 +269,15 @@ document.addEventListener('keydown', event => {
 		playerMove(1);
 	} else if (event.keyCode === 40) {
 		playerDrop();
-	}
-
-	
+	} else if (event.keyCode === 81) {
+		playerRotate(-1);
+	} else if (event.keyCode === 87) {
+		playerRotate(1);
+	}	
 });
 
-
+playerReset();
+updateScore();
 update();
 
 
