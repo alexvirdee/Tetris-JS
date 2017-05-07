e53d3dd5685d5fc4c61f9993631b263eb4f7d5a9
Alexinder Virdee <alex.virdee08@gmail.com>
2017-05-07 01:52:56 -0400

    console log the timing event

--- tetris.js
+++ tetris.js
@@ -3,8 +3,7 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+
 
 const matrix = [
 	[0, 0, 0],
@@ -12,18 +11,53 @@ const matrix = [
 	[0, 1, 0],
 ];
 
-function drawMatrix(matrix, offset) {}
-matrix.forEach((row, y) => {
-	row.forEach((value, x) => {
-		if (value !== 0) {
-			context.fillStyle = 'Red';
-			context.fillRect(x + offset.x, 
-							 y + offset.y, 
-							 1, 1);
-		}
+function draw() {
+	context.fillStyle = '#000';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	drawMatrix(player.matrix, player.pos);
+}
+
+
+
+function drawMatrix(matrix, offset) {
+	matrix.forEach((row, y) => {
+		row.forEach((value, x) => {
+			if (value !== 0) {
+				context.fillStyle = 'Red';
+				context.fillRect(x + offset.x, 
+								 y + offset.y, 
+								 1, 1);
+			}
+		});
 	});
-});
 
 }
 
-drawMatrix(matrix, {x: 5, y: 5});
+let lastTime = 0;
+function update(time = 0) {
+	const deltaTime = time - lastTime;
+	lastTime = time;
+	// console.log(deltaTime);
+	draw();
+	requestAnimationFrame(update);
+}
+
+const player = {
+	pos: {x: 5, y: 5},
+	matrix: matrix,
+}
+
+
+update();
+
+
+
+
+
+
+
+
+
+
+
